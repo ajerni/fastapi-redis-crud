651a7590a481be71041cec16a24a44074a4fdb7b
„Andi <ajerni@gmail.com>
2023-08-16 16:08:46 +0200

    ready for vercel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import Any, List, Dict
 from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import redis
 from redis import RedisError
 import json
@@ -64,6 +66,33 @@ def getallpairs_starswith(key: str):
     return {"messages": pairs}
 
 app = FastAPI(title="FastAPI CURD on Redis")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI CRUD on Redis and Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Andi's FastAPI Test</h1>
+            <h2>Hello from FastAPI</h2>
+            <img src="/static/maneblo_logo.png" alt="maneblo" width="200" height="200">
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
+"""
+
+#root route
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 
 # Create route
 @app.post("/create", tags=["CREATE"])
