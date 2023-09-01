52a213a520dedde1c411b9fe54656db4d018479f
„Andi <ajerni@gmail.com>
2023-09-01 14:19:31 +0200

    ApiKey protection setup added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Security
+from fastapi.security import APIKeyHeader
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from connections.redis_db import REDIS_CLIENT as r
@@ -26,6 +27,33 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Start Api-Key security setup
+api_keys = [
+    "my_api_key",
+    "letmein"
+]
+
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+
+
+def get_api_key(
+        api_key_header: str = Security(api_key_header),
+) -> str:
+    if api_key_header in api_keys:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+# End Api-Key security setup
+
+# protect a route like this:
+@app.get("/protected")
+def private(api_key: str = Security(get_api_key)):
+    """A private endpoint that requires a valid API key to be provided in the header."""
+    return f"Private Endpoint reached sucessfully. API Key: {api_key}"
+
 
 #root route - see frontend
 @app.get("/", tags=["Frontend Landing Page"])
